6a80cd8ddc94e573deddacae73c3609a38edf348
BCGen <bcgenever7@gmail.com>
2018-11-17 14:26:03 +0800

    Create global.d.ts

---
+++ types/global.d.ts
@@ -0,0 +1,16 @@
+interface Window {
+    axios: import('axios').AxiosStatic;
+    Vue: import('vue').VueConstructor;
+}
+
+interface CSRF_TOKEN_TYPE extends Element {
+    content: String;
+}
+
+var axios: typeof window.axios;
+var Vue: typeof window.Vue;
+
+declare module '*.vue' {
+    import Vue from 'vue';
+    export default Vue;
+}
