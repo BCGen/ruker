f9149f7ff548815508a3a40e94eb0231f0c62b78
BCGen <bcgenever7@gmail.com>
2018-11-23 02:16:26 +0800

    update

--- global.d.ts
+++ global.d.ts
@@ -1,16 +1,16 @@
-interface Window {
-  axios: import('axios').AxiosStatic
-  Vue: import('vue').VueConstructor
-}
+// interface Window {
+//   axios: import('axios').AxiosStatic
+//   Vue: import('vue').VueConstructor
+// }
 
-interface CSRF_TOKEN_TYPE extends Element {
-  content: String
-}
+// interface CSRF_TOKEN_TYPE extends Element {
+//   content: String
+// }
 
-var axios: typeof window.axios
-var Vue: typeof window.Vue
+// declare var axios: typeof window.axios
+// declare var Vue: typeof window.Vue
 
-declare module '*.vue' {
-  import Vue from 'vue'
-  export default Vue
-}
+// declare module '*.vue' {
+//   import Vue from 'vue'
+//   export default Vue
+// }
