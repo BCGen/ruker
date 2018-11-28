59afa1bbd88bd8fadea65d2780d2ef8d41a47a71
BCGen <bcgenever7@gmail.com>
2018-11-28 16:17:56 +0800

    Update global.d.ts

--- .vscode/@types/global.d.ts
+++ .vscode/@types/global.d.ts
@@ -1,13 +1,24 @@
-interface Window {
-  [key: string]: any
-}
+// please search and install types in https://microsoft.github.io/TypeSearch/
 
-interface CSRF_TOKEN_TYPE extends Element {
-  content: String
-}
+// npm install --D @types/swiper
+import swiper from 'swiper'
+import popper from 'popper.js'
+import { __ } from 'lodash'
+import { VueConstructor } from 'vue'
+import { AxiosInstance } from 'axios'
 
-declare const _: import('lodash').__
-declare const axios: import('axios').AxiosInstance
-declare const Popper: typeof import('popper.js').default
-declare const Swiper: typeof import('swiper').default
-declare const Vue: import('vue').VueConstructor
+declare global {
+  const _: __
+  const axios: AxiosInstance
+  const Popper: typeof popper
+  const Swiper: typeof swiper
+  const Vue: VueConstructor
+
+  interface Window {
+    [key: string]: any
+  }
+
+  interface CSRF_TOKEN_TYPE extends Element {
+    content: String
+  }
+}
